dc3c25ab116b0071467bde3bfa9e83c0fff18dfa
FreezingTexture22 <andreisesiunin@gmail.com>
2023-06-16 19:50:41 +0200

    Stage 1 completed

--- Stage1.java
+++ Stage1.java
@@ -2,6 +2,23 @@ package Encrypted;
 
 public class Stage1 {
 	public static void main(String[] args) {
+		String input = "we found a treasure!";
+		String anyChar = "[a-z]";
+		StringBuilder output = new StringBuilder();
+		
+		for (int i = 0; i < input.length(); i++ ) {
+			if (input.charAt(i) == ' ' || input.charAt(i) == '!') {
+				output = output.append(input.charAt(i));
+			} else {
+				char tmp = (char) (219 - input.charAt(i));
+				output = output.append(tmp);
+				
+			}
+			
+		}
+		
+		System.out.println(output);
+		
 		
 	}
 }
